9f4f07a95eda3dfa0c7d17cd806ea5ecc17d874c
Shaun Rashid <shaun@mindseyemultimedia.ca>
2017-02-19 14:50:04 -0800

    Updates to models.

--- app/models/dog.js
+++ app/models/dog.js
@@ -1,10 +1,17 @@
 /* jshint node: true */
 
+"use strict";
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var DogSchema = new Schema({
-    name: String
+    name: String,
+	nickname: String,
+	dateOfBirth: Date,
+	dateOfDeath: Date,
+	sire: String,
+	dam: String
 });
 
 module.exports = mongoose.model('Dog', DogSchema);
--- app/models/user.js
+++ app/models/user.js
@@ -1,10 +1,13 @@
 /* jshint node: true */
 
+"use strict";
+
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    name: String
+    name: String,
+	email: String
 });
 
 module.exports = mongoose.model('User', UserSchema);
